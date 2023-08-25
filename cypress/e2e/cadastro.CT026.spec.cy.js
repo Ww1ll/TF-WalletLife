bcd6789ea83ecd8de2ded17a03f5f85b1143f521
holiveira93 <holiveira_93@hotmail.com>
2023-08-25 17:07:30 -0300

    CT026 corrigido

--- cypress/e2e/cadastro.CT026.spec.cy.js
+++ cypress/e2e/cadastro.CT026.spec.cy.js
@@ -2,13 +2,16 @@
 describe('CADASTRAR', () => {
 
     beforeEach(() => {
-        cy.visit("/cadastro")
+        cy.visit("/login")
         //cy.generateFixture()
     });
   
     it('CT026 - Validar botão Logo com sucesso', () => {
-        cy.get('img').click()
-        cy.get('.hero-text > .sc-bcPKhP').contains("Suas finanças, nosso compromisso!")
+        cy.fixture('usuario.data.json').then(data => {
+            cy.efetuarLogin(data.email, data.senha)
+            cy.get('.sc-eLSjS > :nth-child(1) > img').click()
+            cy.get('.hero-text > .sc-bcPKhP').contains("Suas finanças, nosso compromisso!")
+        })
     })
   
   })
