78cc6e1e6ee671d09bebb86d7d20083dedd2b7b1
damtrovao <damtrovao@gmail.com>
2023-08-25 16:24:47 -0300

    Criação do teste de login com usuário inexistente

--- cypress/e2e/login.ct015.spec.cy.js
+++ cypress/e2e/login.ct015.spec.cy.js
@@ -20,6 +20,11 @@ describe('Efetuar Login', () => {
       cy.get('.sc-bcPKhP').should('contain', 'Olá, Fulano')
   })
 
-  
+  it('validar efetuar login com usuário não cadastrado no sistema', () => {
+    cy.fixture('usuario.data.json').then(data => {
+      cy.efetuarLogin(data.usuario[0].email, data.usuario[0].senha)
+      cy.get('.Toastify__toast-body > :nth-child(2)').should('contain', 'Usuário e/ou senha incorretos')
+    })
+  })
 
 })
