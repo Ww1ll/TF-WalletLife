dc0571408f044fc7a10b156eb88ed34fc6dfdca4
Kassia do Espirito Santo <kassiaes@gmail.com>
2023-08-25 16:40:26 -0300

    test home CT06

--- cypress/e2e/home.ct003.spec.cy.js
+++ cypress/e2e/home.ct003.spec.cy.js
@@ -1,4 +1,4 @@
-describe('Efetuar Login', () => {
+describe('Pagina Home', () => {
 
     beforeEach(() => {
         cy.visit("https://wallet-life.vercel.app/")        
@@ -7,7 +7,5 @@ describe('Efetuar Login', () => {
     it('validar botao "Entrar na plataforma!" com sucesso', () => {     
         cy.get('#root > div > div.sc-pqitP.bSzvfc > div.hero-text > a > button').click()           
             cy.get('.sc-bcPKhP').contains('LOGIN')
-    })      
-   
-  
+    })           
 })
