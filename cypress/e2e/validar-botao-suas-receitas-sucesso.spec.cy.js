bc0edcc1a02692430d03143576b00aeffa61c65f
Alexia Sacerdote <alexiasacerdotedeoliveira@gmail.com>
2023-08-25 20:06:17 -0300

    test validar botão receita com sucesso

---
+++ cypress/e2e/validar-botao-suas-receitas-sucesso.spec.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Validar botão Suas Receitas com sucesso', () => {
+
+    beforeEach(() => {
+        cy.visit("/")
+    });
+  
+    it('validar botão suas receitas com sucesso', () => {
+      cy.get('#root > div > header > div > a:nth-child(2) > button').click()
+      cy.fixture('usuario.data.json').then(data => {
+        cy.efetuarLogin(data.email, data.senha)
+      })
+      cy.url().should('eq', 'https://wallet-life.vercel.app/sua-carteira', { timeout: 10000 })
+      cy.get('#root > div > header > div > a:nth-child(2)').click()
+      cy.url().should('eq', 'https://wallet-life.vercel.app/receitas', { timeout: 10000 })
+    })
+  
+  })
