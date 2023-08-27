e0cd1880f9380c8e83ef72ecd04a283d49adb87e
Mateus Aniceto <mateuslaniceto@gmail.com>
2023-08-27 12:51:22 -0300

    feat: Adicionado CT004 - Validar botão entrar na plataforma! com sucesso

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
 import '../support/commands/login.commands.js'
 import '../support/commands/dataFaker.commands.js'
 import '../support/commands/cadastro.commands.js'
+import '../support/commands/home.commands.js'
