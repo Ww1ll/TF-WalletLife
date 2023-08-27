1b250a537376166d49afe929f78fd50c4fe327fa
damtrovao <damtrovao@gmail.com>
2023-08-27 11:46:23 -0300

    Criação do teste do botão de receitas na página de despesas

--- cypress/support/commands/cadastro.commands.js
+++ cypress/support/commands/cadastro.commands.js
@@ -1,5 +1,48 @@
-Cypress.Commands.add('cadastrarUsuario', (email, senha) => {
+Cypress.Commands.add('cadastrarUsuario', (nome, email, dataNascimento, cpf, senha) => {
+    cy.get('#name').type(nome)
     cy.get('#email').type(email)
+    cy.get('#dateBith').type(dataNascimento)
+    cy.get('#cpf').type(cpf)
     cy.get('#password').type(senha)
+    cy.get('.sc-jSwlEQ').click()
+})
+
+Cypress.Commands.add('cadastrarUsuarioSemNome', (email, dataNascimento, cpf, senha) => {
+    cy.get('#email').type(email)
+    cy.get('#dateBith').type(dataNascimento)
+    cy.get('#cpf').type(cpf)
+    cy.get('#password').type(senha)
+    cy.get('.sc-jSwlEQ').click()
+})
+
+Cypress.Commands.add('cadastrarUsuarioSemEmail', (nome, dataNascimento, cpf, senha) => {
+    cy.get('#name').type(nome)
+    cy.get('#dateBith').type(dataNascimento)
+    cy.get('#cpf').type(cpf)
+    cy.get('#password').type(senha)
+    cy.get('.sc-jSwlEQ').click()
+})
+
+Cypress.Commands.add('cadastrarUsuarioSemDataNascimento', (nome, email, cpf, senha) => {
+    cy.get('#name').type(nome)
+    cy.get('#email').type(email)
+    cy.get('#cpf').type(cpf)
+    cy.get('#password').type(senha)
+    cy.get('.sc-jSwlEQ').click()
+})
+
+Cypress.Commands.add('cadastrarUsuarioSemCpf', (nome, email, dataNascimento, senha) => {
+    cy.get('#name').type(nome)
+    cy.get('#email').type(email)
+    cy.get('#dateBith').type(dataNascimento)
+    cy.get('#password').type(senha)
+    cy.get('.sc-jSwlEQ').click()
+})
+
+Cypress.Commands.add('cadastrarUsuarioSemSenha', (nome, email, dataNascimento, cpf) => {
+    cy.get('#name').type(nome)
+    cy.get('#email').type(email)
+    cy.get('#dateBith').type(dataNascimento)
+    cy.get('#cpf').type(cpf)
     cy.get('.sc-jSwlEQ').click()
 })
--- cypress/support/commands/dataFaker.commands.js
+++ cypress/support/commands/dataFaker.commands.js
@@ -1,15 +1,22 @@
 const { pt_BR } = require('faker/lib/locales')
+import {format} from 'date-fns';
 
 Cypress.Commands.add('generateFixture', () => {
 
     var faker = require('faker')
+    var fakerBr = require('faker-br')
     faker.locale='pt_BR'
     cy.writeFile('cypress/fixtures/usuario.data.json', {
       'usuario':Cypress._.times(2, () => {
+        const pastDate = faker.date.past();
+        const formattedPastDate = format(pastDate, 'yyyy-MM-dd');
         return {
+          'nome':`${faker.name.firstName()}`,
           'email':`${faker.internet.email()}`,
+          'dataNascimento':`${formattedPastDate}`,
+          'cpf':`${fakerBr.br.cpf()}`,
           'senha':`${faker.internet.password()}`,
-          'nome':`${faker.internet.userName()}`,
+          
         }
       }),
     })
