107ffbfda2eea030eb233d44703175c4ad559448
Mateus Aniceto <mateuslaniceto@gmail.com>
2023-08-27 11:43:03 -0300

    feat: Adicionado CT008 - Validar Cadastrar Usuário com Sucesso

--- cypress/e2e/cadastro.spec.cy.js
+++ cypress/e2e/cadastro.spec.cy.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress"/>
 
-describe('Efetuar Login', () => {
+describe('Efetuar Cadastro', () => {
 
   beforeEach(() => {
       cy.visit("/cadastro")
+      cy.generateFixture()
   });
 
-  it('validar cadastrar usuário com sucesso', () => {
+  it('CT008 - Validar cadastrar usuário com sucesso', () => {
     cy.fixture('usuario.data.json').then(data => {
-      cy.efetuarLogin(data.email, data.senha)
+      const dataUsuario = data.usuario[0];
+      cy.cadastrarUsuario(dataUsuario.nomeCompleto, dataUsuario.email, dataUsuario.dataNascimento, dataUsuario.cpf, dataUsuario.senha)
     })
   })
-
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,2 +1,3 @@
 import '../support/commands/login.commands.js'
 import '../support/commands/dataFaker.commands.js'
+import '../support/commands/cadastro.commands.js'
