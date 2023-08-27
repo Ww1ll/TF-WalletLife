f01ff5628d4336e6b9760054a69cb80a792319c8
William <109118303+Ww1ll@users.noreply.github.com>
2023-08-27 12:05:47 -0300

    Fix: Ajustes Home-Page-CT005

--- cypress/e2e/HomePage-CT005.spec.cy.js
+++ cypress/e2e/HomePage-CT005.spec.cy.js
@@ -9,8 +9,10 @@ describe('Fale Conosco', () => {
 
     it('Validar Fale Conosco', () => {
         cy.fixture('faleConosco.data.json').then(data => {
-            cy.faleConosco(data.nome, data.email, data.descricao)
+            cy.faleConosco(data.faleConosco[0].nome,  data.faleConosco[0].email, data.faleConosco[0].descricao)
+            cy.get('.styled-input > .Toastify > .Toastify__toast-container > #\\31  > .Toastify__toast-body > :nth-child(2)').should('contain', 'Sua dúvida foi enviada!')
         })
+
 
     })
 })
