7dcc81dc528a093896e85a02452ef5e162e06e6e
damtrovao <damtrovao@gmail.com>
2023-08-27 18:39:39 -0300

    Junção de classes de funcionalidade em classe única - Dashboard receitas

--- cypress/e2e/dashboard.receitas.spec.cy.js
+++ cypress/e2e/dashboard.receitas.spec.cy.js
@@ -1,17 +1,14 @@
 /// <references types="cypress"/>
 
-describe('Botão "+"', () => {
+describe('Dashboard Receitas', () => {
     
     beforeEach(() => {
         cy.visit("/login")
     })
 
-    it('validar botão "+" com sucesso', () => {
+    it('CT028 - Validar botão "+" com sucesso', () => {
         cy.fixture('usuario.data.json').then(data => {
-// 
-// 
-// 
-            cy.efetuarLogin("larissa@teste.com", "123321")
+            cy.criarUsuarioELogarNoSistema(data.usuario[0].nomeCompleto, data.usuario[0].email, data.usuario[0].dataNascimento, data.usuario[0].cpf, data.usuario[0].senha)
             cy.get('.navegacao > [href="/receitas"]').click()
             cy.get('#root > div > section > div.sc-bjEwCx.gxMsbI > div > button').click()
             cy.get('#root > div > div > div > div > h3').contains('ADICIONAR TRANSAÇÃO')
