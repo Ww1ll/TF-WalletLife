178826b6f44b7609cbd55742555427592af20d7d
damtrovao <damtrovao@gmail.com>
2023-08-29 10:55:04 -0300

    Junção de casos de teste

--- cypress/e2e/dashboard.investimentos.spec.cy.js
+++ cypress/e2e/dashboard.investimentos.spec.cy.js
@@ -114,6 +114,27 @@ describe('Dashboard - Investimentos', () => {
         })
     })
 
+    it('CT077 - cenário positivo de atualização de investimento', () => {
+
+        cy.fixture('usuario.data.json').then(data => {
+            cy.clicarBotaoRegistrar()
+            cy.cadastrarUsuario(data.usuario[0].nomeCompleto, data.usuario[0].email, data.usuario[0].dataNascimento, data.usuario[0].cpf, data.usuario[0].senha)
+            cy.wait(3000)
+            cy.efetuarLoginSemMensagem(data.usuario[0].email, data.usuario[0].senha)
+        })
+
+        cy.fixture('transacao.data.json').then(data =>{
+            let tipo = ["Renda variável", "Renda fixa"]
+            let tipoRandom = Math.floor(Math.random() * 2)
+
+            cy.navegarParaTelaDeInvestimento()
+            cy.cadastrarInvestimento(tipo[tipoRandom], data.investimento[0].valor, data.investimento[0].descricao, data.investimento[0].corretora, data.investimento[0].data)
+            cy.get('#root > div > div > div.sc-iUeHef.SrCsm > div > span').click()
+
+            cy.editarInvestimento(tipo[tipoRandom], data.investimento[1].valor, data.investimento[1].descricao, data.investimento[1].corretora, data.investimento[1].data)
+        })
+    })
+
     it('CT078 - cenário negativo de atualização de investimento', () => {
         cy.fixture('usuario.data.json').then(data => {
             cy.efetuarLogin(data.usuario[0].email, data.usuario[0].senha)
